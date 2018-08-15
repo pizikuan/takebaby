b2790e80fb1e82e3624920329b314ab47cb3da81
liyinghao <13375368363@139.com>
2018-08-15 16:55:09 +0800

    将表单提交请求后台方法放到match.js当中

--- client/pages/match/match.js
+++ client/pages/match/match.js
@@ -54,8 +54,9 @@ var app=getApp()
 Page({
   data: {
     title: '匹配',
-    matchRes: [],
-    hidden: 'true'
+    matchRes: null,
+    hidden: 'true',
+    loginErrorCount: 1,
   },
 
   onPullDownRefresh: function() {
@@ -98,9 +99,49 @@ Page({
   onShow: function () {
     console.log("onShow")
   },
-
+  matchSet: function () {
+    this.setData({
+      matchRes: [1, 2, 3, 4]
+    })
+  },
+  //开启匹配后台调用
   match: function () {
+    // post数据给服务器获取匹配结果
+    // console.log(app.globalData.home);
+    // console.log(app.globalData.school);
+    // console.log(app.globalData.parentName);
+    // console.log(app.globalData.wechat);
+    // console.log(app.globalData.mobile);
+  
+    this.data.isShow = true;
+    //后台请求
+    wx.request({
+      url: 'http://192.168.51.10:8888/api/json/match',
+      data: {
+        "userName": JSON.stringify(app.globalData.parentName),
+        "wxNum": JSON.stringify(app.globalData.wechat),
+        "phoneNum": JSON.stringify(app.globalData.mobile),
+        "code": app.globalData.code,
+        "home": JSON.stringify(app.globalData.home),
+        "school": JSON.stringify(app.globalData.school)
+      },
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {// 设置请求的 header
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      success: function (res) {
+        // console.log(JSON.stringify(res.data)
+        // this.data.matchRes = []
+        // this.data.isShow = true;
 
+        that.setData({
+          'loginErrorCount': 0
+        });
+      },
+      fail: function (res) {
+        console.log('error: ' + res)
+      }
+    })
   }
 
 })
