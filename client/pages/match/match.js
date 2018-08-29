80afb3c18098bf4cb3fc6a27e1f21a318de6df81
pizikuan <pizikuan@163.com>
2018-08-29 11:15:36 +0800

    modify match.js

--- client/pages/match/match.js
+++ client/pages/match/match.js
@@ -1,6 +1,5 @@
 var Api = require('../../utils/api.js');
-
-var app=getApp()
+const app = getApp()
 var that;
 
 Page({
@@ -11,19 +10,21 @@ Page({
     isMatched: 'false'
   },
 
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     console.log('onPullDownRefresh', new Date())
   },
 
   // 事件处理函数
-  redictDetail: function(e) {
+  redictDetail: function (e) {
     var id = e.currentTarget.id,
       url = '../detail/detail?id=' + id;
 
-    wx.navigateTo({url: url})
+    wx.navigateTo({
+      url: url
+    })
   },
 
-  onLoad: function (){ 
+  onLoad: function () {
     console.log("match.js - onLoad() - isMathching" + app.globalData.isMatching)
   },
 
@@ -46,40 +47,18 @@ Page({
 
   },
 
-
   //开启匹配后台调用
   match: function () {
-  
+
     // 登录
     wx.login({
-      success: res => {
+      success: function (res) {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        var code = res.code;
+        var code = res.code
         // this.globalData.code = code;
-      //后台请求
-      wx.request({
-        // url: 'http://192.168.51.10:8888/api/json/match',
-        url: app.globalData.host + 'json/getUserInfo',
-        data: {
-          "userName": app.globalData.parentName,
-          "wxNum": app.globalData.wechat,
-          "phoneNum": app.globalData.mobile,
-          "code": code,
-          "home": JSON.stringify(app.globalData.home),
-          "school": JSON.stringify(app.globalData.school)
-        },
-        method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-        header: {// 设置请求的 header
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        success: function (res) {
-          // console.log(JSON.stringify(res.data)
-          // this.data.matchRes = []
-          // this.data.isShow = true;
-          // console.log(res)
         //后台请求
         wx.request({
-          url: app.globalData.host + 'json/match',
+          url: app.globalData.host + 'json/getUserInfo',
           data: {
             "userName": app.globalData.parentName,
             "wxNum": app.globalData.wechat,
@@ -88,62 +67,85 @@ Page({
             "home": JSON.stringify(app.globalData.home),
             "school": JSON.stringify(app.globalData.school)
           },
-          method: 'POST',     // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-          header: {           // 设置请求的 header
+          method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          header: {       // 设置请求的 header
             'content-type': 'application/x-www-form-urlencoded'
           },
+
           success: function (res) {
             // console.log(JSON.stringify(res.data)
             // this.data.matchRes = []
             // this.data.isShow = true;
             // console.log(res)
+            //后台请求
+            wx.request({
+              url: app.globalData.host + 'json/match',
+              data: {
+                "userName": app.globalData.parentName,
+                "wxNum": app.globalData.wechat,
+                "phoneNum": app.globalData.mobile,
+                "code": code,
+                "home": JSON.stringify(app.globalData.home),
+                "school": JSON.stringify(app.globalData.school)
+              },
+              method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+              header: { // 设置请求的 header
+                'content-type': 'application/x-www-form-urlencoded'
+              },
+              success: function (res) {
+                // console.log(JSON.stringify(res.data)
+                // this.data.matchRes = []
+                // this.data.isShow = true;
+                // console.log(res)
 
-            // 删除loading标志
-            that.setData({
-              isMathching : 'false'
-            })
+                // 删除loading标志
+                that.setData({
+                  isMathching: 'false'
+                })
 
-            if (res.data.errno == 1) {
-              console.log("请求失败");
+                if (res.data.errno == 1) {
+                  console.log("请求失败")
 
-              wx.showModal({
-                title: '',
-                content: '请勿重复请求',
-                showCancel: false,
-                success: function (res) {
-                  wx.navigateTo({
-                    url: '../index/index'
+                  wx.showModal({
+                    title: '',
+                    content: '请勿重复请求',
+                    showCancel: false,
+                    success: function (res) {
+                      wx.navigateTo({
+                        url: '../index/index'
+                      })
+                    }
                   })
                 }
-              })
-            } else {
-              that.setData({
-                'matchRes': JSON.parse(res.data.data)
-              });
-            }
-          },
-          fail: function (res) {
-            console.log('error: ' + res)
+                else {
+                  that.setData({
+                    'matchRes': JSON.parse(res.data.data)
+                  });
+                }
+              },
+
+              fail: function (res) {
+                console.log('error: ' + res)
+              }
+            })
           }
+
         })
+      },
+
+      onLoad: function () {
+
+        wx.showShareMenu({
+          withShareTicket: false
+        })
+      },
+
+      onShareAppMessage: function () {
+        return {
+          title: app.globalData.shareTitle,
+          desc: app.globalData.shareDesc,
+          path: "pages/index/index",
+          imageUrl: "/images/share.jpg"
+        }
       }
     })
-  },
-
-  onLoad: function () {
-
-    wx.showShareMenu({
-      withShareTicket: false
-    })
-  },
-
-  onShareAppMessage: function () {
-    return {
-      title: app.globalData.shareTitle,
-      desc: app.globalData.shareDesc,
-      path: "pages/index/index",
-      imageUrl: "/images/share.jpg"
-    }
-  }
-})
-
