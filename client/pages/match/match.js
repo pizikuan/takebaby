58455eb4ef384435533c1067b17656fc74e55559
pizikuan <pizikuan@163.com>
2018-08-29 11:20:15 +0800

    format match.js

--- client/pages/match/match.js
+++ client/pages/match/match.js
@@ -1,6 +1,6 @@
 var Api = require('../../utils/api.js');
 
-var app=getApp()
+var app = getApp()
 var that;
 
 Page({
@@ -11,19 +11,19 @@ Page({
     isMatched: 'false'
   },
 
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     console.log('onPullDownRefresh', new Date())
   },
 
   // 事件处理函数
-  redictDetail: function(e) {
+  redictDetail: function (e) {
     var id = e.currentTarget.id,
       url = '../detail/detail?id=' + id;
 
-    wx.navigateTo({url: url})
+    wx.navigateTo({ url: url })
   },
 
-  onLoad: function (){ 
+  onLoad: function () {
     console.log("match.js - onLoad() - isMathching" + app.globalData.isMatching)
   },
 
@@ -49,29 +49,29 @@ Page({
 
   //开启匹配后台调用
   match: function () {
-  
+
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         var code = res.code;
         // this.globalData.code = code;
-      //后台请求
-      wx.request({
-        // url: 'http://192.168.51.10:8888/api/json/match',
-        url: app.globalData.host + 'json/match',
-        data: {
-          "userName": app.globalData.parentName,
-          "wxNum": app.globalData.wechat,
-          "phoneNum": app.globalData.mobile,
-          "code": code,
-          "home": JSON.stringify(app.globalData.home),
-          "school": JSON.stringify(app.globalData.school)
-        },
-        method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-        header: {// 设置请求的 header
-          'content-type': 'application/x-www-form-urlencoded'
-        },success: function (res) {
+        //后台请求
+        wx.request({
+          // url: 'http://192.168.51.10:8888/api/json/match',
+          url: app.globalData.host + 'json/match',
+          data: {
+            "userName": app.globalData.parentName,
+            "wxNum": app.globalData.wechat,
+            "phoneNum": app.globalData.mobile,
+            "code": code,
+            "home": JSON.stringify(app.globalData.home),
+            "school": JSON.stringify(app.globalData.school)
+          },
+          method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          header: {// 设置请求的 header
+            'content-type': 'application/x-www-form-urlencoded'
+          }, success: function (res) {
             // console.log(JSON.stringify(res.data)
             // this.data.matchRes = []
             // this.data.isShow = true;
@@ -79,7 +79,7 @@ Page({
 
             // 删除loading标志
             that.setData({
-              isMathching : 'false'
+              isMathching: 'false'
             })
 
             if (res.data.errno == 1) {
